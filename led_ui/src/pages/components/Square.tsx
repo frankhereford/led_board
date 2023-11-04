2f854b7ac33e6139597e9f8324cd58bcf1ca84fa
Frank Hereford <frank@frankhereford.com>
2023-11-04 23:07:37 +0000

    ok using signals now

--- led_ui/src/pages/components/Square.tsx
+++ led_ui/src/pages/components/Square.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { signal } from "@preact/signals-react";
 
 interface SquareProps {
   x: number;
   y: number;
 }
 
+const color = signal([0,0,0])
+
 const Square: React.FC<SquareProps> = ({ x, y }) => {
-  const [r, setR] = React.useState<number>(0);
-  const [g, setG] = React.useState<number>(0);
-  const [b, setB] = React.useState<number>(0);
+
+  // Function to generate an array of 3 random numbers between 0 and 255
+  const randomizeColor = () => {
+    color.value = [0, 1, 2].map(() => Math.floor(Math.random() * 256));
+  };
 
   // Ensure x and y are positive integers
   if (x < 0 || y < 0 || !Number.isInteger(x) || !Number.isInteger(y)) {
@@ -20,8 +25,9 @@ const Square: React.FC<SquareProps> = ({ x, y }) => {
       style={{
         width: '100px',
         height: '100px',
-        backgroundColor: `rgb(${r}, ${g}, ${b})`,
+        backgroundColor: `rgb(${color.value[0]}, ${color.value[1]}, ${color.value[2]})`,
       }}
+      onClick={randomizeColor} // Attached randomizeColor to the onClick event
     >
     </div>
   );
