4a5b431ee8daf679a9e943e3bc5b74b178aeab87
Frank Hereford <frank@frankhereford.com>
2023-11-04 21:14:42 +0000

    make 2d

--- redis_experiment/push_ordered_values.py
+++ redis_experiment/push_ordered_values.py
@@ -5,7 +5,10 @@ import time
 # Initialize a single Redis client connection
 redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
-def generate_tuples(step=5):
+w, h = 24, 24  # for a grid of 24x24 LEDs
+
+
+def generate_tuples(step=3):
     for r in range(0, 256, step):
         yield (r, 0, 0)
     for g in range(0, 256, step):
@@ -19,26 +22,28 @@ def generate_tuples(step=5):
     for b in reversed(range(0, 256, step)):
         yield (0, 0, b)
 
-
-
-def set_display_value(i, value):
+def set_display_value(x, y, value):
+    key = f'display:{x}:{y}'
     with redis_client.pipeline() as pipeline:
         pipeline.multi()
-        pipeline.delete(f'display:{i}')
-        pipeline.rpush(f'display:{i}', *value)
+        pipeline.delete(key)
+        pipeline.rpush(key, *value)
         pipeline.execute()
+
 
 def main():
     tuple_generator = generate_tuples()
     while True:
-        for i in range(577):
-            try:
-                value = next(tuple_generator)
-            except StopIteration:
-                tuple_generator = generate_tuples()
-                value = next(tuple_generator)
-            set_display_value(i, value)
-            time.sleep(.05)
+        for y in range(h):
+            for x in range(w):
+                try:
+                    value = next(tuple_generator)
+                except StopIteration:
+                    tuple_generator = generate_tuples()
+                    value = next(tuple_generator)
+                set_display_value(x, y, value)
+                # Control the speed of updates to prevent overwhelming Redis
+                time.sleep(.05)
 
 if __name__ == "__main__":
     main()
