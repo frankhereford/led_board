7b0c995b26b7a1731356f5b1d60ccc6bf465930c
Frank Hereford <frank@frankhereford.com>
2023-11-05 01:33:46 +0000

    like not a problem we're having

--- led_ui/src/pages/components/Square.tsx
+++ led_ui/src/pages/components/Square.tsx
@@ -30,11 +30,6 @@ const Square: React.FC<SquareProps> = ({ x, y }) => {
     setSquareColor(inks.value[0]!)
   };
 
-  // Ensure x and y are positive integers
-  if (x < 0 || y < 0 || !Number.isInteger(x) || !Number.isInteger(y)) {
-    return <div>Props x and y must be positive integers.</div>;
-  }
-
   return (
     <div
       style={{
