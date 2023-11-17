9d6d8b77dd9e9b753c0573097601fe0d4758b160
Frank Hereford <frankhereford@users.noreply.github.com>
2023-11-17 17:04:36 -0600

    this feels done

--- blender/import_lights.py
+++ blender/import_lights.py
@@ -17,7 +17,6 @@ def empty_scene():
 
     # Delete all collections except the master collection
     for collection in bpy.data.collections:
-        #if collection.name != "Collection":
         bpy.data.collections.remove(collection)
 
     # Delete all cameras
@@ -30,17 +29,54 @@ def empty_scene():
 
     print("Cleared the Blender scene.")
 
-def create_icosphere(name, coordinates, size, subdivisions):
-    bpy.ops.mesh.primitive_ico_sphere_add(radius=size, subdivisions=subdivisions, location=(coordinates['x'], coordinates['y'], coordinates['z']))
+def create_icosphere(name, coordinates, size, subdivisions, collection):
+    # Multiply x and y coordinates by 10
+    scaled_location = (coordinates['x'] * 10, coordinates['y'] * 10, coordinates['z'])
+    
+    bpy.ops.mesh.primitive_ico_sphere_add(radius=size, subdivisions=subdivisions, location=scaled_location)
     obj = bpy.context.active_object
     obj.name = name
 
+    collection.objects.link(obj)  # Link the icosphere to the specified collection
+    bpy.context.collection.objects.unlink(obj)
+    return obj
+
+def assign_emission_material(obj, color=(1, 1, 1), strength=5.0):
+    # Create a new material
+    mat = bpy.data.materials.new(name="EmissionMaterial")
+
+    # Enable 'Use nodes':
+    mat.use_nodes = True
+    nodes = mat.node_tree.nodes
+
+    # Clear all nodes to start clean
+    nodes.clear()
+
+    # Create an Emission node
+    emission_node = nodes.new(type='ShaderNodeEmission')
+    emission_node.location = (0, 0)
+    emission_node.inputs['Color'].default_value = (*color, 1)
+    emission_node.inputs['Strength'].default_value = strength
+
+    # Create an Output node
+    output_node = nodes.new('ShaderNodeOutputMaterial')
+    output_node.location = (200, 0)
+
+    # Link Emission to Output
+    links = mat.node_tree.links
+    links.new(emission_node.outputs['Emission'], output_node.inputs['Surface'])
+
+    # Assign it to the object
+    if obj.data.materials:
+        # Replace material
+        obj.data.materials[0] = mat
+    else:
+        # Add material
+        obj.data.materials.append(mat)
 
 
-def load_data(data):
+def load_data(data, max_lights_per_group=5000):
     # Iterate over the JSON data and create objects in Blender
-    print(data)
-
     for ip in data:
         if ip not in bpy.data.collections:
             ip_collection = bpy.data.collections.new(ip)
@@ -49,29 +85,14 @@ def load_data(data):
             print(group)
             group_collection = bpy.data.collections.new(group)
             ip_collection.children.link(group_collection)
+            lights_added = 0
             for light in data[ip][group]:
-                create_icosphere(f"{light['index']}", light['coordinate'], 0.1, 10)
-                pass
-                #print(light)
-                #create_object(f"{ip}_{group}_{light['index']}", light['coordinate'])
+                if lights_added >= max_lights_per_group:
+                    break
+                sphere = create_icosphere(f"{ip}-{group}-{light['index']}", light['coordinate'], 0.05, 1, group_collection)
+                assign_emission_material(sphere, color=(0, 1, 0), strength=10)
+                lights_added += 1
 
-    return
-    for ip, collections in data.items():
-        for collection_name, objects in collections.items():
-            # Create a new collection if it doesn't exist
-            if collection_name not in bpy.data.collections:
-                new_collection = bpy.data.collections.new(collection_name)
-                bpy.context.scene.collection.children.link(new_collection)
-
-            # Select the collection
-            collection = bpy.data.collections[collection_name]
-
-            # Create objects in the collection
-            for obj in objects:
-                pass
-                #create_object(f"{collection_name}_{obj['index']}", obj['coordinate'])
-                # Link the object to the collection
-                #collection.objects.link(bpy.context.active_object)
 
 empty_scene()
 load_data(lights)
