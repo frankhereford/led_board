95e7f8eaa5c6be55630e7b6a408d0a7db0a1787a
Frank Hereford <frankhereford@users.noreply.github.com>
2023-11-20 22:38:16 -0600

    fix up export script

--- blender/export_empties.py
+++ blender/export_empties.py
@@ -8,13 +8,24 @@ for collection in bpy.context.scene.collection.children:
     for sub_collection in collection.children:
         data[collection.name][sub_collection.name] = []
         for obj in sub_collection.objects:
+            # Split the object name over the colon
+            print(obj.name)
+            split_name = obj.name.split('-')
+
+            # Convert the value after the colon to an integer and store it in a variable
+            if len(split_name) > 1:
+                number = int(split_name[1])
+            else:
+                number = None  # or some default value if no colon is found
+
             # Dividing x and y coordinates by 10
             coords = obj.location
             data[collection.name][sub_collection.name].append({
+                "index": number,  # add the integer value to the data structure
                 "coordinates": {"x": coords.x / 10, "y": coords.y / 10, "z": coords.z}
             })
 
-print(data)
+#print(data)
 
 # Specifying the file path
 file_path = '/Users/frank/Development/led_board/data/installation.json'
