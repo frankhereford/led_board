fe9fbe26f640b518d61e2eabc4578362b4e0b099
Frank Hereford <frankhereford@users.noreply.github.com>
2023-11-22 23:03:14 -0600

    lint

--- visualizer/render.py
+++ visualizer/render.py
@@ -4,31 +4,32 @@ import json
 import time
 import pygame
 
+r = redis.Redis(host="10.10.10.1", port=6379, db=0)
 
-r = redis.Redis(host='10.10.10.1', port=6379, db=0)
 
 def parse_data(data):
     points = []
     data_dict = json.loads(data)  # Assuming data is a JSON string
 
-    #print(data_dict)
+    # print(data_dict)
     if not data_dict:
         return points
 
     for _, nested_data in data_dict.items():
         for _, points_list in nested_data.items():
             for point in points_list:
-                x = ((point['coordinate']['x'] + 1) * 400) + 10
-                y = 600 - ((point['coordinate']['y'] * 600) + 10)
+                x = ((point["coordinate"]["x"] + 1) * 400) + 10
+                y = 600 - ((point["coordinate"]["y"] * 600) + 10)
                 color = (0, 0, 0)
-                if 'color' in point:
+                if "color" in point:
                     color = (
-                        point['color']['r'],
-                        point['color']['g'],
-                        point['color']['b'],
+                        point["color"]["r"],
+                        point["color"]["g"],
+                        point["color"]["b"],
                     )
                 points.append((x, y, color))
     return points
+
 
 # Initialize Pygame
 pygame.init()
@@ -37,7 +38,6 @@ pygame.init()
 screen_width, screen_height = 800, 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Real-Time Data Visualization")
-
 
 # Main loop flag
 running = True
@@ -48,16 +48,18 @@ while running:
             running = False
 
     # Retrieve and parse data
-    data = r.get('installation_layout')
+    data = r.get("installation_layout")
     if data:
         points = parse_data(data)
 
         # Clear screen
-        screen.fill((255, 255, 255))  # Fill the screen with white
+        screen.fill((0, 0, 0))
 
         # Draw points
         for x, y, color in points:
-            pygame.draw.circle(screen, color, (int(x), int(y)), 4)  # 5 is the radius of the circle
+            pygame.draw.circle(
+                screen, color, (int(x), int(y)), 4
+            )  # 5 is the radius of the circle
 
     # Update the screen
     pygame.display.flip()
