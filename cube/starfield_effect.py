7b38a896a480f94158edc6a826a0306300d73f86
Frank Hereford <frankhereford@users.noreply.github.com>
2023-11-26 19:09:16 -0600

    rainbow starfield

--- cube/starfield_effect.py
+++ cube/starfield_effect.py
@@ -6,6 +6,8 @@ import pygame
 import random
 import json
 
+import colorsys
+
 # Initialize Pygame
 pygame.init()
 
@@ -16,9 +18,9 @@ clock = pygame.time.Clock()
 
 # Star properties
 num_stars = 300
-star_speed = 1.4 
-#camera_y_offset = 80  # pan
-#camera_translation_y = -50  # translate
+star_speed = 1.4
+# camera_y_offset = 80  # pan
+# camera_translation_y = -50  # translate
 camera_y_offset = 0  # pan
 camera_translation_y = -30  # translate
 
@@ -64,33 +66,49 @@ class Star:
         self.x = random.randint(-width, width)
         self.y = random.randint(-height, height)
         self.z = random.randint(1, width)
+        self.color = next(color_generator)  # Assign a color using the generator
 
     def update(self):
         self.z -= star_speed
         if self.z <= 0:
             self.x = random.randint(-width, width)
-            self.y = (
-                random.randint(-height, height) - camera_y_offset
-            )  # Subtracting the offset here
+            self.y = random.randint(-height, height) - camera_y_offset
             self.z = width
+            self.color = next(color_generator)  # Update color when a star is reset
 
     def show(self, screen):
         x, y = self.x / self.z, self.y / self.z
         x = int(x * width / 2 + width / 2)
-        y = (
-            int(y * height / 2 + height / 2) - camera_translation_y
-        )  # Applying translation here
+        y = int(y * height / 2 + height / 2) - camera_translation_y
 
         if 0 <= x < width and 0 <= y < height:
-            pygame.draw.circle(screen, (255, 255, 255), (x, y), max(1, int(5 / self.z)))
+            pygame.draw.circle(screen, self.color, (x, y), max(1, int(5 / self.z)))
 
 
-# Create stars
+# Generator function for cycling through hues
+def cycle_hue():
+    hue = 0
+    while True:
+        rgb_color = colorsys.hls_to_rgb(hue / 360, 0.5, 1)
+        pygame_color = tuple(int(255 * i) for i in rgb_color)
+        yield pygame_color
+        hue = (hue + 1) % 360
+
+
+# Create a color generator instance
+color_generator = cycle_hue()
+
 stars = [Star() for _ in range(num_stars)]
 
 
 layout = layout_library.read_json_from_file("installation_v2_groups.json")
 light_positions = precompute_light_positions(layout, width, height)
+
+# Initialize the trail surface
+trail_surface = pygame.Surface((width, height), pygame.SRCALPHA)
+
+# Define the fade amount
+fade_amount = 10  # Adjust as needed for the desired fade effect
 
 
 # Main loop
@@ -100,11 +118,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((0, 0, 0))
+    trail_surface.fill((0, 0, 0, fade_amount))
 
     for star in stars:
         star.update()
         star.show(screen)
+
+    screen.blit(trail_surface, (0, 0))
 
     for (x, y), lights in light_positions.items():
         # Invert the y-coordinate to correct the upside-down issue
